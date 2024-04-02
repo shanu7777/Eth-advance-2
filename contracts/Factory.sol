388e9fe192cac7b88be0d4d7b657514d4e925f4a
shanu7777 <165034258+shanu7777@users.noreply.github.com>
2024-04-02 23:54:33 +0530

    Update Factory.sol

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -7,30 +7,33 @@ import "./CollateralProtection.sol";
 
 contract InsuranceFactory {
     mapping(address => address) private userInsuranceContracts;
+    mapping(address => address) private userWalletContracts;
 
-    mapping(address => address) private userWalletContract;
- event WalletInsuranceCreated(address indexed user, address walletInsurance);
+    event WalletInsuranceCreated(address indexed user, address walletInsurance);
     event CollateralProtectionCreated(address indexed user, address collateralProtection);
 
-    function createWalletInsurance(uint256 _insuredAmount) external {
-        require(userWalletContract[msg.sender] == address(0), "You already have an existing insurance contract.");
+    function createWalletInsurance(uint256 insuredAmount) external {
+        require(userWalletContracts[msg.sender] == address(0), "You already have an existing insurance contract.");
 
-        WalletInsurance walletInsurance = new WalletInsurance(_insuredAmount);
-
-        userWalletContract[msg.sender] = address(walletInsurance);
+        WalletInsurance walletInsurance = new WalletInsurance(insuredAmount);
+        userWalletContracts[msg.sender] = address(walletInsurance);
 
         emit WalletInsuranceCreated(msg.sender, address(walletInsurance));
     }
 
     function getUserWalletContract() external view returns (address) {
-        return userWalletContract[msg.sender];
+        return userWalletContracts[msg.sender];
     }
+
     function createCollateralProtection() external {
         require(userInsuranceContracts[msg.sender] == address(0), "You already have an existing insurance contract.");
+
         CollateralProtection collateralProtection = new CollateralProtection();
         userInsuranceContracts[msg.sender] = address(collateralProtection);
+
         emit CollateralProtectionCreated(msg.sender, address(collateralProtection));
     }
+
     function getUserInsuranceContracts() external view returns (address) {
         return userInsuranceContracts[msg.sender];
     }
